afcb48754084d56bc4c82ca74de104f7308eb5d7
Subhash Bhalerao <subhash-devopslearner@gmail.com>
2026-03-18 15:38:48 +0530

    Update Dockerfile

--- djk8s/settings.py
+++ djk8s/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class HomeTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome to the Home Page')
